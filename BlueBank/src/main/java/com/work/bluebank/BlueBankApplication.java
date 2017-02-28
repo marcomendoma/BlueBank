ffd85926b796bb5c3f983f392ac949beb0f03c13
Marcomendoma <marcomendoma@ig.com.br>
2017-02-28 13:33:29 -0300

    Inicio do projeto

---
+++ BlueBank/src/main/java/com/work/bluebank/BlueBankApplication.java
@@ -0,0 +1,12 @@
+package com.work.bluebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueBankApplication.class, args); 
+	}
+}
